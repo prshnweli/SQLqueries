2d054d3fc309f3165bbd769f4da28d0a671297ac
Prashan Welipitiya <prshn.weli@gmail.com>
2019-07-17 09:58:47 -0700

    updated test collumns

--- PBISRound2.sql
+++ PBISRound2.sql
@@ -3,6 +3,7 @@ SELECT
 "First Name" = STU.FN,
 "Last Name" = STU.LN,
 Grade = STU.GR,
+School = (SELECT LOC.NM FROM LOC WHERE STU.SC = LOC.CD),
 Gender = STU.SX,
 Foster = CASE WHEN (SELECT TOP(1) CD FROM PGM WHERE (STU.ID = PGM.PID) AND (PGM.CD = 190)) = '190' THEN 'Y' ELSE '' END,
 Homeless = CASE WHEN (SELECT TOP(1) CD FROM PGM WHERE (STU.ID = PGM.PID) AND (PGM.CD = 191)) = '191' THEN 'Y' ELSE '' END,
@@ -10,9 +11,10 @@ LangFluency = STU.LF,
 "Long Term EL" = CASE WHEN (SELECT YP FROM LAC WHERE (STU.ID = LAC.ID)) > 3 THEN 'Y' ELSE '' END,
 SocioEcoStatus =  case when stu.id in (select fre.id from fre where fre.id = stu.id and fre.cd in ( 'f', 'r' ) and  (fre.esd > '7/1/2018' and fre.eed = '6/30/2019' ) and fre.del = 0 and ( stu.ped = 14 or fre.cd is not null )) then 'Y' else 'N' end,
 SPED = STU.U2,
-MAP_Lexile = ISNULL ((SELECT TOP (1) OT AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'MAP') AND (PT = 1)) ORDER BY TD DESC ),''),
-MAP_ELA = ISNULL ((SELECT TOP (1) SS AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'MAP') AND (PT = 1)) ORDER BY TD DESC ),''),
-MAP_Math = ISNULL ((SELECT TOP (1) SS AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'MAP') AND (PT = 20)) ORDER BY TD DESC ),''),
+MAP_ELA = NULLIF(ISNULL ((SELECT TOP (1) PC AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'MAP') AND (PT = 1) ORDER BY TD DESC ),''), 0),
+MAP_ELA_Score = NULLIF(ISNULL ((SELECT TOP (1) SS AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'MAP') AND (PT = 1) ORDER BY TD DESC ),''), 0),
+MAP_Math = NULLIF(ISNULL ((SELECT TOP (1) PC AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'MAP') AND (PT = 20) ORDER BY TD DESC ),''), 0),
+MAP_Math_Score = NULLIF(ISNULL ((SELECT TOP (1) SS AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'MAP') AND (PT = 20) ORDER BY TD DESC ),''), 0),
 "Days Suspended" = ISNULL ((SELECT TOP (1) SU AS score FROM AHS WHERE (YR >= '2010-2011') AND (YR <= '2018-2019') AND (AHS.ID = stu.ID) ORDER BY SU DESC ),''),
 Expelled = CASE WHEN (SELECT TOP (1) EXD FROM EXP WHERE (STU.ID = EXP.PID) ORDER BY EXD DESC) != '' THEN 'Y' ELSE '' END,
 [Race] = (CASE
@@ -24,7 +26,7 @@ Expelled = CASE WHEN (SELECT TOP (1) EXD FROM EXP WHERE (STU.ID = EXP.PID) ORDER
 	WHEN STU.RC1 = 700 THEN 'White'
 	ELSE 'Unknown'
 END),
-"5150 ideations" = '?',
+"5150 ideations" = CASE WHEN (SELECT TOP(1) CD FROM CNF WHERE (STU.ID = CNF.PID) AND (CNF.CD = 'AH')) = 'AH' THEN 'Y' ELSE '' END,
 [ChronicAbs] = CASE WHEN
 CAST(
 	((SELECT CAST(SUM(AHS.PR) AS FLOAT) FROM AHS WHERE (AHS.ID = STU.ID) AND AHS.YR = '2018-2019'))/((SELECT CAST(SUM(AHS.EN) AS FLOAT) FROM AHS WHERE (AHS.ID = STU.ID) AND AHS.YR = '2018-2019'))
@@ -33,12 +35,12 @@ ELSE '' END,
 Truant = ISNULL ((SELECT SUM(AU) FROM AHS WHERE (YR = '2018-2019') AND (AHS.ID = stu.ID)),''),
 Tardies = ISNULL((SELECT SUM(TD) FROM AHS WHERE (YR = '2018-2019') AND (AHS.ID = STU.ID)),''),
 "Number of Credits" = STU.CC,
-Oral_Perf =  ISNULL ((SELECT TOP (1) cast( PL AS INT) AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'ELPAC') AND (PT = 1) ORDER BY TD DESC ),''),
-Written_Perf =  ISNULL ((SELECT TOP (1) cast(PL AS INT) AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'ELPAC') AND (PT = 2) ORDER BY TD DESC ),''),
-Listening_Perf =  ISNULL ((SELECT TOP (1) cast( PL AS INT) AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'ELPAC') AND (PT = 3) ORDER BY TD DESC ),''),
-Speaking_Perf =  ISNULL ((SELECT TOP (1) cast( PL AS INT) AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'ELPAC') AND (PT = 4) ORDER BY TD DESC ),''),
-Reading_Perf =  ISNULL ((SELECT TOP (1) cast( PL AS INT) AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'ELPAC') AND (PT = 5) ORDER BY TD DESC ),''),
-Writing_Perf =  ISNULL ((SELECT TOP (1) cast( PL AS INT) AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'ELPAC') AND (PT = 6) ORDER BY TD DESC ),''),
+Oral_Perf =  NULLIF(ISNULL ((SELECT TOP (1) cast( PL AS INT) AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'ELPAC') AND (PT = 1) ORDER BY TD DESC ),''), 0),
+Written_Perf =  NULLIF(ISNULL ((SELECT TOP (1) cast(PL AS INT) AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'ELPAC') AND (PT = 2) ORDER BY TD DESC ),''), 0),
+Listening_Perf =  NULLIF(ISNULL ((SELECT TOP (1) cast( PL AS INT) AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'ELPAC') AND (PT = 3) ORDER BY TD DESC ),''), 0),
+Speaking_Perf =  NULLIF(ISNULL ((SELECT TOP (1) cast( PL AS INT) AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'ELPAC') AND (PT = 4) ORDER BY TD DESC ),''), 0),
+Reading_Perf =  NULLIF(ISNULL ((SELECT TOP (1) cast( PL AS INT) AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'ELPAC') AND (PT = 5) ORDER BY TD DESC ),''), 0),
+Writing_Perf =  NULLIF(ISNULL ((SELECT TOP (1) cast( PL AS INT) AS score FROM TST WHERE (DEL = 0) AND (PID = stu.ID) AND (ID = 'ELPAC') AND (PT = 6) ORDER BY TD DESC ),''), 0),
 GPA = STU.TP,
 Retained = CASE WHEN (SELECT CD FROM RET WHERE (RET.PID = STU.ID) AND (RET.CD = 'R')) = 'R' THEN 'Y' ELSE '' END
 FROM STU
